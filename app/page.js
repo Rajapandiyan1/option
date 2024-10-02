cc0bd92e32934dc3d69bcd8d6dc9be6523ea250e
Raja Pandiyan <rajapandiyan1163@gmail.com>
2024-10-02 17:43:18 +0530

    app start

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
     <h1>Wellcome with tailwindCss</h1>
+    <p className="text-blue-900"> ipsum dolor sit amet consectetur adipisicing elit. Et nisi vitae, quisquam hic debitis id fuga cum blanditiis ad, modi ipsa voluptatem non sit dolores mollitia aliquid, obcaecati libero deserunt? ipsum dolor sit amet consectetur adipisicing elit. Et nisi vitae, quisquam hic debitis id fuga cum blanditiis ad, modi ipsa voluptatem non sit dolores mollitia aliquid, obcaecati libero deserunt?</p>
     </>
   );
 }
